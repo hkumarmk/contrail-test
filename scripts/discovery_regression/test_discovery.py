0cb551a1db165d606226de0b258a83e19f9454bb
Kamlesh Parmar <kparmar@juniper.net>
2016-02-13 09:09:29 -0800

    Partial-Bug :#1545306 disable webui testcases in discovery module
    Change-Id: I11eb218c1d4787b4cea8a1340db1cbb087bb90cf

--- scripts/discovery_regression/test_discovery.py
+++ scripts/discovery_regression/test_discovery.py
@@ -176,7 +176,7 @@ class TestDiscovery(BaseDiscoveryTest):
         resp = self.ds_obj.cleanup_service_from_discovery(self.inputs.cfgm_ip)
         return True
 
-    @test.attr(type=['sanity', 'ci_sanity', 'vcenter'])
+    @test.attr(type=['sanity', 'vcenter'])
     @preposttest_wrapper
     def test_control_nodes_subscribed_to_ifmap_service(self):
         '''
@@ -192,7 +192,7 @@ class TestDiscovery(BaseDiscoveryTest):
                 ip)
         return True
 
-    @test.attr(type=['sanity', 'ci_sanity', 'vcenter'])
+    @test.attr(type=['sanity', 'vcenter'])
     @preposttest_wrapper
     def test_webui_subscribed_to_opserver_service(self):
         ''' Validate webui subscribed to opserver service
